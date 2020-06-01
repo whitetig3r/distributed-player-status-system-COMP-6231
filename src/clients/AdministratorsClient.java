25a3c3136084101a250f6b64df90add8d47dc8ae
Warren White <wwhite@samasource.org>
2020-05-31 21:44:01 -0400

    add interactive CLI

--- src/clients/AdministratorsClient.java
+++ src/clients/AdministratorsClient.java
@@ -26,9 +26,37 @@ public class AdministratorsClient {
 	public static void main(String[] args) {
 		// TODO Build an interactive menu driven UI
 		System.out.println("NOTE -- Admin Logs available at " + System.getProperty("user.dir") + "/admin_logs");
-		adminSignIn();
-		adminGetPlayerStatus();
-		adminSignOut();
+		final String MENU_STRING = "\n-- Admin Client CLI --\n"
+				+ "Pick an option ...\n"
+				+ "1. Sign in with admin privileges\n"
+				+ "2. Sign out admin\n"
+				+ "3. Get status of all players playing the game\n"
+				+ "4. Exit the CLI\n"
+				+ "--------------------------\n";
+		while(true) {
+			System.out.println(MENU_STRING);
+			switch(sc.nextLine()) {
+				case "1": {
+					adminSignIn();
+					break;
+				}
+				case "2": {
+					adminSignOut();
+					break;
+				}
+				case "3": {
+					adminGetPlayerStatus();
+					break;
+				}
+				case "4": {
+					System.out.println("Goodbye!");
+					System.exit(0);
+				}
+				default: {
+					System.out.println("Invalid Option selected!");
+				}
+			}
+		}
 	}
 	
 	private static void adminSignIn() {
--- src/clients/PlayersClient.java
+++ src/clients/PlayersClient.java
@@ -18,16 +18,44 @@ import server.GameServerRMI;
 public class PlayersClient {
 
 	private static Scanner sc = new Scanner(System.in);
-	private static GameServerRMI serverStub;
+	private static GameServerRMI serverStub; 
 	
 
 	public static void main(String[] args) {
+		final String MENU_STRING = "\n-- Player Client CLI --\n"
+				+ "Pick an option ...\n"
+				+ "1. Create a Player account\n"
+				+ "2. Sign a Player in\n"
+				+ "3. Sign a Player out\n"
+				+ "4. Exit the CLI\n"
+				+ "--------------------------\n";
 		try {
 			// TODO Build an interactive menu driven UI
-			System.out.println("NOTE -- Player Logs available at " + System.getProperty("user.dir") + "/player_logs");
-			createPlayerAccount();
-			playerSignIn();
-			playerSignOut();
+			System.out.println("NOTE -- Player Logs available at " + System.getProperty("user.dir") + "/player_logs/");
+			while(true) {
+				System.out.println(MENU_STRING);
+				switch(sc.nextLine()) {
+					case "1": {
+						createPlayerAccount();
+						break;
+					}
+					case "2": {
+						playerSignIn();
+						break;
+					}
+					case "3": {
+						playerSignOut();
+						break;
+					}
+					case "4": {
+						System.out.println("Goodbye!");
+						System.exit(0);
+					}
+					default: {
+						System.out.println("Invalid Option selected!");
+					}
+				}
+			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -127,8 +155,10 @@ public class PlayersClient {
 		 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		 LocalDateTime tStamp = LocalDateTime.now(); 
 		 String writeString = String.format("[%s] %s @ (%s) -- %s", dtf.format(tStamp), uName, ipAddress, logStatement);
+		 int registryPort = ClientUtilities.getRegionServer(ipAddress);
+		 String serverRegion = ClientUtilities.getServerName(registryPort);
 		 try{
-			File file = new File(String.format("player_logs/%s.log", uName));
+			File file = new File(String.format("player_logs/%s/%s.log", serverRegion.substring(1), uName));
 			file.getParentFile().mkdirs();
 			FileWriter fw = new FileWriter(file, true);
 			BufferedWriter logger = new BufferedWriter(fw);
